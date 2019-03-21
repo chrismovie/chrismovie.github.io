23ef29b57dab3f0017e9f176149c3be110e8aee6
Krzysztof Płonka <info@krzysztofplonka.pl>
2019-03-21 19:39:49 +0100

    back to top2

--- _site/js/back-to-top.js
+++ _site/js/back-to-top.js
@@ -1,39 +1,9 @@
-var $j = jQuery.noConflict();
-
-$j(document).ready(function() {
-	"use strict";
-
-function totop_button(a) {
-	"use strict";
-
-	var b = $j("#back_to_top");
-	b.removeClass("off on");
-	if (a === "on") { b.addClass("on"); } else { b.addClass("off"); }
-}
-
-function backButtonShowHide(){
-	"use strict";
-
-	$j(window).scroll(function () {
-		var b = $j(this).scrollTop();
-		var c = $j(this).height();
-		var d;
-		if (b > 0) { d = b + c / 2; } else { d = 1; }
-		if (d < 1e3) { totop_button("off"); } else { totop_button("on"); }
-	});
-}
-
-function backToTop(){
-	"use strict";
-
-	$j(document).on('click','#back_to_top',function(e){
-		e.preventDefault();
-
-		$j('body,html').animate({scrollTop: 0}, $j(window).scrollTop()/3, 'linear');
-	});
-}
-
-//backButtonShowHide();
-backToTop();
-
+$(window).scroll(function() {
+    if ($(this).scrollTop() > 50 ) {
+        $('.scrolltop:hidden').stop(true, true).fadeIn();
+    } else {
+        $('.scrolltop').stop(true, true).fadeOut();
+    }
 });
+
+$(function(){$(".post_to_top").click(function(){$("html,body").animate({scrollTop:$("#top").offset().top},"1000");return false})})
